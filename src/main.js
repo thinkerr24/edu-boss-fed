4a448c056510b7eb0e5db92d7b451a0e05d9d0bf
thinkerr24 <thinkerr24@163.com>
2024-03-25 23:21:33 +0800

    样式处理

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import store from './store'
 // 引入elementUI
 import ElementUI from 'element-ui'
 // 引入elementUI的主题(风格)文件
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
+// 引入自定义的全局样式文件
+import './styles/index.scss'
 
 // 将elementUI注册为Vue插件
 Vue.use(ElementUI)
